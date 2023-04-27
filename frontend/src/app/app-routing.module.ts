b98e7cb60355257b0ac293d9fd63afb36a5ff770
Omar Zunic <contact@omarzunic.com>
2023-04-27 18:32:43 +0200

    Cleanup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
-import { FrontPageComponent } from '../front-page/front-page.component';
-import { AboutComponent } from '../about/about.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { FrontPageComponent } from './components/front-page/front-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: FrontPageComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './components/app/app.component';
+import { AppComponent } from './components/app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterOutlet } from '@angular/router';
-import { AppRoutingModule } from './components/app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FrontPageComponent } from './components/front-page/front-page.component';
 import { AboutComponent } from './components/about/about.component';
@@ -12,7 +12,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, FrontPageComponent, AboutComponent, HeaderComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FrontPageComponent,
+    AboutComponent,
+    HeaderComponent,
+    DashboardComponent,
+  ],
   imports: [BrowserModule, RouterOutlet, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
