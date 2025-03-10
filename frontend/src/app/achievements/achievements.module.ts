33a1cde501fe4297188fe9bf174237048bad42f7
Omar Zunic <contact@omarzunic.com>
2025-03-10 16:26:24 +0100

    Implement translation support for achievements component and add achievements module
    Signed-off-by: Omar Zunic <contact@omarzunic.com>

---
+++ frontend/src/app/achievements/achievements.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AchievementsComponent } from "./achievements.component";
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+	declarations: [AchievementsComponent],
+	imports: [CommonModule, TranslateModule],
+})
+export class AchievementsModule {}
